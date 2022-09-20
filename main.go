313332ffaa4851240b80c4755cea89f461d7d47f
bwelboren <52087548+bwelboren@users.noreply.github.com>
2022-09-20 11:55:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,47 +1,40 @@
 package main
 
+// A simple program demonstrating the text input component from the Bubbles
+// component library.
+
 import (
 	"fmt"
-	"os"
+	"log"
 
-	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-var (
-	textStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("252")).Render
-	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
-	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
+var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type (
+	tickMsg struct{}
+	errMsg  error
 )
 
 type model struct {
+	textInput textinput.Model
 	err       error
 	window    int
-	Books     []string
-	selected  map[int]struct{} // which to-do items are selected
-	cursor    int
-	spinner   spinner.Model
-	textInput textinput.Model
+	books     struct{ name []string }
 }
 
-type errMsg struct{ err error }
-
-// e = errMsg
-// e has err = Error
-// error.Error
-func (e errMsg) Error() string { return e.err.Error() }
-
-func (m model) Init() tea.Cmd {
-	return tea.Batch(m.spinner.Tick, textinput.Blink)
-}
-
-func InitModel() model {
-	sm := spinner.New()
-	sm.Style = spinnerStyle
-	sm.Spinner = spinner.Dot
-
+func initialModel() model {
 	ti := textinput.New()
 	ti.Placeholder = "Brave New World"
 	ti.Focus()
@@ -50,123 +43,91 @@ func InitModel() model {
 
 	return model{
 		textInput: ti,
-		spinner:   sm,
 		err:       nil,
 	}
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
 
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
-	//var cmds []tea.Cmd
-
-	// Controleer de type van msg
 	switch msg := msg.(type) {
-
-	case errMsg:
-		m.err = msg
-		return m, tea.Quit
-
-	// Als de type is dat er op een knop gedrukt is
-	// Kijk welke knop zodra CTRL+C sluit dan af
 	case tea.KeyMsg:
-
-		switch msg.String() {
-		case "ctrl+c", "q":
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
-		case "ctrl+b", "b":
-			m.window = 1
-		// Add book
-		case "d":
+		case tea.KeyEnter:
+			if m.window == 0 {
+				m.window = 1
+				m.books.name = append(m.books.name, m.textInput.Value())
+				m.textInput.Blur()
+				m.textInput.Reset()
+				return m, cmd
+			}
+
+		}
+		switch msg.String() {
+		//Add book
+		case "ctrl+a":
+			m.window = 0
+			m.textInput.Focus()
+			// Show books
+		case "ctrl+b":
 			m.window = 2
+			m.textInput.Blur()
+			//Main menu
+		case "ctrl+x":
+			m.window = 3
+			m.textInput.Blur()
 		}
 
-		// case "up", "k":
-		// 	if m.cursor > 0 {
-		// 		m.cursor--
-		// 	}
-
-		// // The "down" and "j" keys move the cursor down
-		// case "down", "j":
-		// 	if m.cursor < len(m.Books)-1 {
-		// 		m.cursor++
-		// 	}
-
-		// // The "enter" key and the spacebar (a literal space) toggle
-		// // the selected state for the item that the cursor is pointing at.
-		// case "enter", " ":
-		// 	_, ok := m.selected[m.cursor]
-		// 	if ok {
-		// 		delete(m.selected, m.cursor)
-		// 	} else {
-		// 		m.selected[m.cursor] = struct{}{}
-		// 	}
-		// }
-
-	case spinner.TickMsg:
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
-
-	default:
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
 		return m, nil
 	}
 
 	m.textInput, cmd = m.textInput.Update(msg)
-
-	// Als we geen ander bericht krijgen doe dan niks
 	return m, cmd
 }
 
-func (m model) View() (s string) {
+func (m model) View() string {
 
-	s = "\n• Welcome •\n"
+	s := ""
 
-	s += fmt.Sprintf("\n %s%s%s\n\n", m.spinner.View(), " ", textStyle("Running..."))
-	footer := helpStyle("- d: add book • b: show books • q: exit\n")
+	if m.window == 0 {
+		s = fmt.Sprintf(
+			"Enter the name of the book you want to add.\n\n%s\n\n",
+			m.textInput.View(),
+		)
 
-	if m.err != nil {
-		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
 	}
-
-	// Laat gebruiker zien dat we bezig zijn
-
 	if m.window == 1 {
-		s += "\nYour books:\n"
 
-		for i, choice := range m.Books {
+		s = fmt.Sprintf(
+			"You added a book named:\n\n%s\n\n",
+			m.books.name[len(m.books.name)-1],
+		) + "\n"
 
-			// Is the cursor pointing at this choice?
-			cursor := " " // no cursor
-			if m.cursor == i {
-				cursor = ">" // cursor!
-			}
+	}
+	if m.window == 2 {
 
-			// Is this choice selected?
-			checked := " " // not selected
-			if _, ok := m.selected[i]; ok {
-				checked = "x" // selected!
-			}
+		s = "\n\nYour library:\n\n"
 
-			// Render the row
-			s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		for _, book := range m.books.name {
+			s += fmt.Sprintf("%s\n", book)
 		}
 	}
 
-	if m.window == 2 {
-		s += fmt.Sprintf(
-			"What’s your favorite Book?\n\n%s\n\n%s",
-			m.textInput.View(), "\n")
+	if m.window == 3 {
+		mb := m.books.name
+		s = fmt.Sprintf("\n\n%d , %d\n\n", len(mb), cap(mb))
 	}
 
-	return "\n" + s + "\n\n" + footer
+	footer := helpStyle("- ctrl+a: add book • ctrl+b: show books • ctrl+c: exit\n")
+	return s + footer
 
-}
-
-func main() {
-
-	if err := tea.NewProgram(InitModel()).Start(); err != nil {
-		fmt.Printf("Uh oh, there was an error: %v\n", err)
-		os.Exit(1)
-	}
 }
